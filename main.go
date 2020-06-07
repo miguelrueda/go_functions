7ab49a518831d8274aeb846beacd18602cde4764
miguel.rueda@outlook.com <$_mrueda91>
2020-06-07 15:39:19 -0500

    Multiple return

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("%f", divide(6, 3))
+	// Use _ to ignore a return value
+	// To use _,_ it is used = instead of :=
+	answer, err := divide(6, 3)
+	if err != nil {
+		fmt.Printf("An error occurred %s\n", err.Error())
+	} else {
+		fmt.Printf("%f\n", answer)
+	}
 }
 
-func divide(p1, p2 float64) float64 {
-	return p1 / p2
+func divide(p1, p2 float64) (float64, error) {
+	if p2 == 0 {
+		return math.NaN(), errors.New("Cannot divide by zero")
+	}
+	return p1 / p2, nil
 }
 
 func add(p1, p2 float64) float64 {
