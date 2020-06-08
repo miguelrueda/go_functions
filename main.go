8bf3ca761f5d2f7bc86e39410cfda5822e4a5467
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-06-08 18:43:39 -0500

    Module 4 compelted

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"functions/simplemath"
+	"io"
 	"math"
 	"net/http"
 	"strings"
@@ -69,6 +71,84 @@ func main() {
 	for _, f := range funcs {
 		println(f())
 	}
+
+	// ReadSomethingBad()
+	if err := ReadFullFile(); err != nil {
+		fmt.Printf("something bad occurred %s", err)
+	}
+}
+
+func ReadFullFile() (err error) {
+	var r io.ReadCloser = &SimpleReader{}
+	defer func() {
+		_ = r.Close()
+		if p := recover(); p == errCatastrophicReader {
+			println(p)
+			err = errors.New("A panic occurred but it is ok")
+		} else if p != nil {
+			panic("an unexpected error occurred and we do not want to recover")
+		}
+	}()
+
+	defer func() {
+		println("before for-loop")
+	}()
+	for {
+		value, readerErr := r.Read([]byte("text that does nothing"))
+		if readerErr == io.EOF {
+			println("finished reading file, breaking out of the loop")
+			break
+		} else if readerErr != nil {
+			return readerErr
+		}
+		println(value)
+	}
+	defer func() {
+		println("after for-loop")
+	}()
+	return nil
+}
+
+func ReadSomethingBad() error {
+	var r io.Reader = BadReader{errors.New("My nonsense reader")}
+	_, err := r.Read([]byte("test something"))
+	if err != nil {
+		fmt.Printf("an error occured %s", err)
+		return err
+	}
+
+	return nil
+}
+
+type BadReader struct {
+	err error
+}
+
+func (br BadReader) Read(p []byte) (n int, err error) {
+	return -1, br.err
+}
+
+type SimpleReader struct {
+	count int
+}
+
+var errCatastrophicReader = errors.New("something catastrphic occurred in the reader")
+
+func (br *SimpleReader) Read(p []byte) (n int, err error) {
+	// println(br.count)
+	if br.count == 2 {
+		panic(errors.New("another error"))
+	}
+	if br.count > 3 {
+		return 0, io.EOF
+	}
+	br.count += 1
+	return br.count, nil
+}
+
+func (br *SimpleReader) Close() error {
+	println("Closing reader")
+	return nil
 }
 
 type RoundTripCounter struct {
@@ -92,9 +172,10 @@ func mathExpression(expr MathExpr) func(float64, float64) float64 {
 	case MultiplyExpr:
 		return simplemath.Multiply
 	default:
-		return func(f float64, f2 float64) float64 {
-			return 0
-		}
+		// return func(f float64, f2 float64) float64 {
+		// 	return 0
+		// }
+		panic("an invalid math expression was used")
 	}
 }
 
